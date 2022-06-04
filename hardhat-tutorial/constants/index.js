c32f614683224943986d50e71c4c18f86cdd8d04
Ekomobong Wisdom <wisdomn200@gmail.com>
2022-06-04 21:22:32 +0100

    backend built by Wisdo N on 04/june/2022 at 9:22 pm. my thirs succesfull web# project

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x9c933e55cc2655cf767B4E150876789211c3b72D";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
